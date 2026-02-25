34d24154565b28e52d6db213ab9421fb6aabd036
Gudama1000 <143605401+Gudama1000@users.noreply.github.com>
2026-02-25 15:21:58 +0100

    Created script for overriding weapon stats
    The script has searialzefield for each weapon stat, at start it sets the stats in a selected weapons genome to those stats

---
+++ Assets/Scripts/GameplayScripts/ShipComponents/WeaponStats.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+public class WeaponStats : MonoBehaviour
+{
+    [SerializeField] Weapon weaponScript;
+
+    // Combat Profile
+    [SerializeField] int baseDamage;
+    [SerializeField] int burstSize;
+    [SerializeField] float fireRate;
+    [SerializeField] float cooldownTime;
+    [SerializeField] float projectileSpeed;
+    [SerializeField] float accuracy;
+    [SerializeField] float spreadAngle;
+    [SerializeField] float range;
+
+    // Resource & Economy
+    [SerializeField] int powerCost;
+    [SerializeField] int heatPerShot;
+    [SerializeField] float heatDissipation;
+    [SerializeField] float chargeUpTime;
+
+    //// Spatial & Synergy
+    //public Vector2 tileFootprint;
+    //public Vector2 tileAffinity;
+    //// public SynergyTag synergyTag;
+
+    // Special Effects
+    //public EffectType statusEffectType;
+    [SerializeField] float statusEffectStrength;
+    [SerializeField] float aoeRadius;
+    //public float piercingDepth;
+
+
+    private void Start()
+    {
+        weaponScript.weapon_Genome.baseDamage = baseDamage;
+        weaponScript.weapon_Genome.burstSize = burstSize;
+        weaponScript.weapon_Genome.fireRate = fireRate;
+        weaponScript.weapon_Genome.cooldownTime = cooldownTime;
+        weaponScript.weapon_Genome.projectileSpeed  = projectileSpeed;
+        weaponScript.weapon_Genome.accuracy = accuracy;
+        weaponScript.weapon_Genome.spreadAngle = spreadAngle;
+        weaponScript.weapon_Genome.range = range;
+
+        weaponScript.weapon_Genome.powerCost = powerCost;
+        weaponScript.weapon_Genome.heatPerShot = heatPerShot;
+        weaponScript.weapon_Genome.heatDissipation = heatDissipation;
+        weaponScript.weapon_Genome.chargeUpTime = chargeUpTime;
+
+        weaponScript.weapon_Genome.statusEffectStrength = statusEffectStrength;
+        weaponScript.weapon_Genome.aoeRadius = aoeRadius;
+    }
+}

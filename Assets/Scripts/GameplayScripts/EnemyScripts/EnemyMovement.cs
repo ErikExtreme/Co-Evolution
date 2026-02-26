7e9ec1098f354175970460b6617e1cc1f372e238
Gudama1000 <143605401+Gudama1000@users.noreply.github.com>
2026-02-26 13:59:40 +0100

    Enemy can now move to a destination
    When it has a destination it:
         -Moves forward
         -Turns to face the destination

--- Assets/Scripts/GameplayScripts/EnemyScripts/EnemyMovement.cs
+++ Assets/Scripts/GameplayScripts/EnemyScripts/EnemyMovement.cs
@@ -1,16 +1,40 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class EnemyMovement : PlayerShip
+public class EnemyMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    //Stats
+    [SerializeField] float speed;
+    [SerializeField] float turnRate;
+    [SerializeField] float mass;
+    [SerializeField] float inertia;
+
+    [SerializeField] float angularDamping = 0.05f;
+
+    private Rigidbody2D rigidbodyThis;
+
+    private Vector2? destination;
+
     void Start()
     {
-        
+        rigidbodyThis = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (destination.HasValue)
+        {
+            Vector2 target = destination.Value;
+
+            Vector2 targetDirection = (target - (Vector2)transform.position).normalized;
+            float targetAngle = Mathf.Atan2(targetDirection.y, targetDirection.x) * Mathf.Rad2Deg - 90f;
+            float angleDifference = Mathf.DeltaAngle(rigidbodyThis.rotation, targetAngle);
+            rigidbodyThis.AddTorque(angleDifference * turnRate - rigidbodyThis.angularVelocity * angularDamping, ForceMode2D.Force);
+
+
+            rigidbodyThis.linearVelocity = Vector2.zero;
+            rigidbodyThis.MovePosition(rigidbodyThis.position + (Vector2)transform.up * speed * Time.fixedDeltaTime);
+        }
     }
 }

67122e63e95712abf137acd82a2db90c20af7da9
ErikExtreme <vilmer@juvin.se>
2026-03-04 12:38:30 +0100

    first outline for the evolutionary loop

--- Assets/Scripts/EvolutionScripts/EvolutionManager.cs
+++ Assets/Scripts/EvolutionScripts/EvolutionManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class EvolutionManager : MonoBehaviour
 {
     public int initalPopulationSize;
+    public int burstPopSize;
+    public int generations;
 
     public List<WeaponGenome> weapons;
     public List<ShipGenome> shipModules;
@@ -27,7 +29,7 @@ public class EvolutionManager : MonoBehaviour
         // 1. Get Player Tracker
         PlayerBehaviorTracker playerTracker = PlayerBehaviorTracker.Instance;
 
-        // 2. Calculate Fitness for all Weapons and Ship Modules
+        // 2. Calculate Individual Fitness for all Weapons and Ship Modules
         foreach (var w in weapons)
         {
             Weapon weapon = WeaponManager.Instance.GetWeapon(w.id);
@@ -42,5 +44,71 @@ public class EvolutionManager : MonoBehaviour
                 return;
             s.fitness = Fitness.IndividualFitness(s, module.tracker, playerTracker);
         }
+
+        // 3. Clone the top X from the global population
+        var weaponPop = weapons.OrderByDescending(g => g.fitness).Take(burstPopSize).Select(g => g.Clone()).ToList();
+        var shipPop = shipModules.OrderByDescending(g => g.fitness).Take(burstPopSize).Select(g => g.Clone()).ToList();
+
+        // 4. Run the Evolutionary Loop
+        for (var gen = 0; gen < generations; gen++)
+        {
+            var nextGenW = new List<WeaponGenome>();
+            var nextGenS = new List<ShipGenome>();
+
+            // 4a. Elitism
+            var eliteW = GetElite(weaponPop);
+            nextGenW.Add(eliteW);
+            var eliteS = GetElite(shipPop);
+            nextGenS.Add(eliteS);
+
+            // 4b. Fill rest of Population
+            while (nextGenW.Count < burstPopSize)
+            {
+                var parent = SelectParent(nextGenW);
+                var child = Mutate(parent);
+                child.fitness = Fitness.CooperativeFitness(child, shipPop, playerTracker);
+                nextGenW.Add(child);
+            }
+            while (nextGenS.Count < burstPopSize)
+            {
+                var parent = SelectParent(nextGenS);
+                var child = Mutate(parent);
+                child.fitness = Fitness.CooperativeFitness(child, weaponPop, playerTracker);
+                nextGenS.Add(child);
+            }
+
+            weaponPop = nextGenW;
+            shipPop = nextGenS;
+        }
+    }
+
+    private WeaponGenome SelectParent(List<WeaponGenome> genomes)
+    {
+        return genomes[0].Clone();
+    }
+
+    private ShipGenome SelectParent(List<ShipGenome> genomes)
+    {
+        return genomes[0].Clone();
+    }
+
+    private WeaponGenome Mutate(WeaponGenome genome)
+    {
+        return genome.Clone();
+    }
+
+    private ShipGenome Mutate(ShipGenome genome)
+    {
+        return genome.Clone();
+    }
+
+    private WeaponGenome GetElite(List<WeaponGenome> genomes)
+    {
+        return genomes[0];
+    }
+
+    private ShipGenome GetElite(List<ShipGenome> genomes)
+    {
+        return genomes[0];
     }
 }
--- Assets/Scripts/EvolutionScripts/Genomes.cs
+++ Assets/Scripts/EvolutionScripts/Genomes.cs
@@ -32,6 +32,33 @@ public class WeaponGenome
     public float statusEffectStrength;
     public float aoeRadius;
     //public float piercingDepth;
+
+    public WeaponGenome Clone() => new WeaponGenome(this);
+
+    public WeaponGenome(WeaponGenome other)
+    {
+        id = 0;
+        fitness = other.fitness;
+        mapping = other.mapping;
+        baseDamage = other.baseDamage;
+        burstSize = other.burstSize;
+        fireRate = other.fireRate;
+        cooldownTime = other.cooldownTime;
+        projectileSpeed = other.projectileSpeed;
+        accuracy = other.accuracy;
+        spreadAngle = other.spreadAngle;
+        range = other.range;
+        powerCost = other.powerCost;
+        heatPerShot = other.heatPerShot;
+        heatDissipation = other.heatDissipation;
+        chargeUpTime = other.chargeUpTime;
+        tileFootprint = other.tileFootprint;
+        tileAffinity = other.tileAffinity;
+        statusEffectStrength = other.statusEffectStrength;
+        aoeRadius = other.aoeRadius;
+    }
+
+    public WeaponGenome() { }
 }
 
 public class ShipGenome
@@ -73,4 +100,33 @@ public class ShipGenome
     public DefenceTag defenceBonusTag;
     public UtilityTag utilityBonusTag;
      */
+
+    public ShipGenome Clone() => new ShipGenome(this);
+
+    public ShipGenome(ShipGenome other)
+    {
+        id = 0;
+        fitness = other.fitness;
+        mapping = other.mapping;
+        hullHP = other.hullHP;
+        armor = other.armor;
+        shieldCapacity = other.shieldCapacity;
+        shieldRegen = other.shieldRegen;
+        powerCapacity = other.powerCapacity;
+        powerRegen = other.powerRegen;
+        speed = other.speed;
+        turnRate = other.turnRate;
+        evasion = other.evasion;
+        mass = other.mass;
+        inertia = other.inertia;
+        gridWidth = other.gridWidth;
+        gridHeight = other.gridHeight;
+        specialTileDensity = other.specialTileDensity;
+        droneCount = other.droneCount;
+        droneSpeed = other.droneSpeed;
+        droneDurability = other.droneDurability;
+        droneAggression = other.droneAggression;
+    }
+
+    public ShipGenome() { }
 }

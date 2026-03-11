8df68664502e94af6ccecfab8b562760b6187844
Gudama1000 <143605401+Gudama1000@users.noreply.github.com>
2026-03-11 19:47:45 +0100

    scripts for ui module/weapon no longer sets their own stats

--- Assets/Scripts/GameplayScripts/ShipDesigning/UIShipModule.cs
+++ Assets/Scripts/GameplayScripts/ShipDesigning/UIShipModule.cs
@@ -13,35 +13,35 @@ public class UIShipModule : MonoBehaviour, IGrabbableUI
 
     Text statsTextBox;
 
-    private void Start()
-    {
-        shipGenome = new ShipGenome();
-        //shipGenome = GlobalSettings.RandomShipGenome();
+    //private void Start()
+    //{
+    //    shipGenome = new ShipGenome();
+    //    //shipGenome = GlobalSettings.RandomShipGenome();
 
-        //Temporary(?) default values
-        shipGenome.hullHP = 1;
-        shipGenome.armor = 1;
-        shipGenome.shieldCapacity = 1;
-        shipGenome.shieldRegen = 1;
-        shipGenome.powerCapacity = 1;
-        shipGenome.powerRegen = 1;
+    //    //Temporary(?) default values
+    //    shipGenome.hullHP = 1;
+    //    shipGenome.armor = 1;
+    //    shipGenome.shieldCapacity = 1;
+    //    shipGenome.shieldRegen = 1;
+    //    shipGenome.powerCapacity = 1;
+    //    shipGenome.powerRegen = 1;
 
-        shipGenome.speed = 4;
-        shipGenome.turnRate = 1.2f;
-        //shipGenome.evasion = 0;
-        //shipGenome.mass = 1;
-        //shipGenome.inertia = 1;
+    //    shipGenome.speed = 4;
+    //    shipGenome.turnRate = 1.2f;
+    //    //shipGenome.evasion = 0;
+    //    //shipGenome.mass = 1;
+    //    //shipGenome.inertia = 1;
 
-        shipGenome.gridWidth = 3;
-        shipGenome.gridHeight = 3;
-        //shipGenome.specialTileDensity
+    //    shipGenome.gridWidth = 3;
+    //    shipGenome.gridHeight = 3;
+    //    //shipGenome.specialTileDensity
 
-        shipGenome.droneCount = 1;
-        shipGenome.droneSpeed = 2;
-        shipGenome.droneDurability = 1;
-        //float droneAggression;
+    //    shipGenome.droneCount = 1;
+    //    shipGenome.droneSpeed = 2;
+    //    shipGenome.droneDurability = 1;
+    //    //float droneAggression;
 
-    }
+    //}
     public void Initialize(ShipGenome shipGenome,ModuleStatsTracker moduleStatsTracker, Text textBox)
     {
         this.shipGenome = shipGenome;

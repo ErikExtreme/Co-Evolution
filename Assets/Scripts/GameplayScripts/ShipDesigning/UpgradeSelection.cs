429d5384fadde05d0d5ebd46d042ba5f5ed48456
Gudama1000 <143605401+Gudama1000@users.noreply.github.com>
2026-03-11 19:13:44 +0100

    Stats are displayed for potential upgrades when hovered

--- Assets/Scripts/GameplayScripts/ShipDesigning/UpgradeSelection.cs
+++ Assets/Scripts/GameplayScripts/ShipDesigning/UpgradeSelection.cs
@@ -1,11 +1,13 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeSelection : MonoBehaviour
 {
     [SerializeField] EvolutionManager evolutionManager;
     [SerializeField] Transform weaponSelectionParent;
     [SerializeField] Transform moduleSelectionParent;
+    [SerializeField] Text statsTextBox;
 
     [SerializeField] GameObject uiWeaponPrefab;
     [SerializeField] GameObject uiModulePrefab;
@@ -42,10 +44,15 @@ public class UpgradeSelection : MonoBehaviour
             GameObject weaponInstance = Instantiate(uiWeaponPrefab, weaponSelectionParent);
             upgrades.weapons[i].id = WeaponGenome.GetNextWeaponId();
             weaponInstance.GetComponent<UIWeapon>().weaponGenome = upgrades.weapons[i];
+            WeaponStatsTracker weaponStatsTracker = new WeaponStatsTracker();
+            UIWeapon weaponScript = weaponInstance.GetComponent<UIWeapon>();
+            weaponScript.Initialize(upgrades.weapons[i], weaponStatsTracker, statsTextBox);
 
             GameObject moduleInstance = Instantiate(uiModulePrefab, moduleSelectionParent);
             upgrades.modules[i].id = ShipGenome.GetNextShipId();
-            moduleInstance.GetComponent<UIShipModule>().shipGenome = upgrades.modules[i];
+            ModuleStatsTracker moduleStatsTracker = new ModuleStatsTracker();
+            UIShipModule moduleScript = moduleInstance.GetComponent<UIShipModule>();
+            moduleScript.Initialize(upgrades.modules[i], moduleStatsTracker, statsTextBox);
         }
     }
 }

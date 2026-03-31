65544a1b7a723007a35f6279a19e0b92cd165c6f
Gudama1000 <143605401+Gudama1000@users.noreply.github.com>
2026-03-31 19:32:33 +0200

    Drones inherit from shipHealth
    Also removed their phyisical collision

--- Assets/Scripts/GameplayScripts/Drone.cs
+++ Assets/Scripts/GameplayScripts/Drone.cs
@@ -1,18 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Drone : MonoBehaviour
+public class Drone : ShipHealth
 {
     //Stats
     float speed;
-    int durability;
     float aggression;
-
 
     //Variables
     private Rigidbody2D rigidbodyThis;
     Vector2 targetPosition;
-    float damage_Taken;
 
     float minimum_Distance_To_Target = 1f;
 
@@ -36,10 +33,10 @@ public class Drone : MonoBehaviour
             Vector3 playerEnemyMidPoint = Vector3.Lerp(shipTransform.position, closestEnemy.position + closestEnemy.up * 0.4f, aggression);
 
             //if (Vector2.Distance(targetPosition, playerEnemyMidPoint) >= 1f)
-                targetPosition = playerEnemyMidPoint;
+            targetPosition = playerEnemyMidPoint;
         }
 
-        Move_To_Target(targetPosition+randomPositionOffset);
+        Move_To_Target(targetPosition + randomPositionOffset);
         rigidbodyThis.linearVelocity = Vector2.zero;
     }
     private void Move_To_Target(Vector2 target_Position)
@@ -54,16 +51,21 @@ public class Drone : MonoBehaviour
     public void SetStats(float speed, int durability, float aggression, ShipDroneManager shipDroneManager)
     {
         this.speed = speed;
-        this.durability = durability;
         this.aggression = aggression * 0.9f + 0.1f;//remaps 0-1 range to 0.1-1 range
 
         this.shipDroneManager = shipDroneManager;
-    }
-    public void TakeDamage(int damage)
-    {
-        damage_Taken += damage;
 
-        if (damage_Taken >= durability)
-            shipDroneManager.DestroyDrone(this);
+
+        HullHP = durability*3;
+        armor = 0;
+        ShieldCapacity = 0;
+        shieldRegen = 0;
+        PowerCapacity = 10;
+        powerRegen = 10;
+        evasion = 0;
+    }
+    protected override void OutOfHealth()
+    {
+        shipDroneManager.DestroyDrone(this);
     }
 }
